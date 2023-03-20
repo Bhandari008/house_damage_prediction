ab20ad15ad01aa49e80105f08a91e029cc61c9c4
Muskan Bhandari <bmuskan007@gmail.com>
2023-03-20 20:40:52 +0545

    created notebook for eda

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import  logging
 
 def error_message_detail(error, error_detail):
     _,_,exec_tb=error_detail.exc_info()
@@ -20,9 +20,3 @@ class CustomException(Exception):
         return self.error_message
 
 
-if __name__== "__main__":
-    try:
-        a=1/0
-    except Exception as e:
-        logging.info("Divide by Zero")
-        raise CustomException(e, sys)
